34620539481857ddf930203986f6a6f8e0dbd3ab
pearlnova <pearlnova.antony@ab-inbev.com>
2026-02-20 18:09:07 +0530

    Fix Docker configuration: Add artifacts to API container and environment variable support for Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
+import os
 import requests
 import streamlit as st
 
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = os.getenv("API_BASE", "http://127.0.0.1:8000")
 
 st.set_page_config(page_title="News Similarity Finder", page_icon="📰", layout="centered")
 st.title("📰 News Article Similarity Finder")
